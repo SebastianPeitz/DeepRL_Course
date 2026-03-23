55cff51667dc40134df859fcac487474ff46bf7f
Sebastian Peitz <52794507+SebastianPeitz@users.noreply.github.com>
2026-03-23 23:47:29 +0100

    new website version

--- docs/public/support/vendor/mathjax-fonts/mathjax-asana-font/cjs/chtml/bold.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.bold = void 0;
-exports.bold = {
-    0x131: [.471, .003, .332],
-    0x237: [.471, .266, .332, { sk: -.051 }]
-};
-//# sourceMappingURL=bold.js.map
